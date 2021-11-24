0113278aafdc085b4d72d68b4fbd988f08acfbce
sa1avci <sa1avci@hotmail.com>
2021-11-25 00:56:54 +0300

    Aspect

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,5 +1,8 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -21,9 +24,10 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
-
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
+            
             if (car.CarName.Length<2)
             {
                 return new ErrorResult(Messages.UnitNameInvalid);
